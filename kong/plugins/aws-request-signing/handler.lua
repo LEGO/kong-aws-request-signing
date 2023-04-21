93cfc919d25f678d59d39c65c11fb1bb93e8efe7
DanielRailean <daniel.railean1@gmail.com>
2023-04-21 09:53:38 +0200

    Update handler.lua

--- kong/plugins/aws-request-signing/handler.lua
+++ kong/plugins/aws-request-signing/handler.lua
@@ -65,7 +65,7 @@ local function get_iam_credentials(sts_conf, refresh)
 
   if err then
     kong.log.err(err)
-    return kong.response.exit(401, { message = "Unable to get the IAM credentials! Check token!" })
+    return kong.response.exit(401, { message = err })
   end
 
   if not iam_role_credentials
@@ -79,7 +79,7 @@ local function get_iam_credentials(sts_conf, refresh)
     )
     if err then
       kong.log.err(err)
-      return kong.response.exit(401, { message = "Unable to refresh expired IAM credentials! Check token!" })
+      return kong.response.exit(401, { message = err })
     end
     kong.log.debug("expiring key , invalidated iam_cache and fetched fresh credentials!")
   end
